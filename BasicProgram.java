a31d6c24980bf4c650b44b82223b26026120e266
Tejaswinig-27 <66554551+Tejaswinig-27@users.noreply.github.com>
2021-08-03 10:54:43 +0530

    commit my changes

---
+++ BasicProgram.java
@@ -0,0 +1,11 @@
+package Firstday;
+
+public class BasicProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!!");
+		System.out.println("p");
+	}
+
+}
